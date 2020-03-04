f270a96dc75455f153d9593d35cb9f2d47bef2b7
Nguyen Ngoc Ban <nguyen.ngoc.ban@sun-asterisk.com>
2020-03-04 15:42:45 +0700

    Add speech result layout

--- app/src/main/java/vn/sunasterisk/english_conversations/constant/Constant.java
+++ app/src/main/java/vn/sunasterisk/english_conversations/constant/Constant.java
@@ -6,6 +6,11 @@ public class Constant {
     public static final String REQUEST_METHOD_GET = "GET";
     public static final int CONNECT_TIME_OUT = 5000;
     public static final int READ_TIME_OUT = 5000;
+    public static final int TIMER_DELAY = 100;
+    public static final int MAX_SCORE = 100;
+    public static final int VERY_GOOD_SCORE = 90;
+    public static final int GOOD_SCORE = 60;
+    public static final int ONE_HUNDRED = 100;
     public static final String BREAK_LINE = "\n";
     public static final String BASE_URL = "https://english-conversation-160713.web.app";
     public static final String DATA_JSON_URL = StringUtils.formatFromBaseURL("data.json");
@@ -15,5 +20,6 @@ public class Constant {
     public static final String SLASH = "/";
     public static final String COMMA = ",";
     public static final String DOT = ".";
+    public static final String TWO_DOT = ":";
     public static final String EMPTY_STRING = "";
 }
